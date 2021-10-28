9466227772c0fffe4848eadd2bbf6cfe18d5e45a
GHOST1119 <82262108+GHOST1119@users.noreply.github.com>
2021-10-28 19:07:42 +0330

    update1

---
+++ WebApplication3/Repository/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Repository
+{
+    interface IAccountRepository
+    {
+        bool Insert(string userName,string email,string password);
+    }
+}
---
+++ WebApplication3/Services/AccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication3.Repository;
+
+namespace WebApplication3.Services
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public bool Insert(string userName, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
